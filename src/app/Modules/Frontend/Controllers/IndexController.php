5a411dc67389c51a7edc2b14307425f5dfa695ce
someson <somevov4ik@gmail.com>
2022-09-23 14:53:48 +0200

    bump Phalcon version to 5.0.0 stable

--- src/app/Modules/Frontend/Controllers/IndexController.php
+++ src/app/Modules/Frontend/Controllers/IndexController.php
@@ -3,12 +3,13 @@
 namespace App\Modules\Frontend\Controllers;
 
 use Phalcon\Support\Version;
+use Phalcon\Tag;
 
 class IndexController extends ControllerBase
 {
     public function indexAction()
     {
-        \Phalcon\Tag::setTitle('phalcon 5.x index page');
+        Tag::setTitle('phalcon 5.x index page');
 
         $css = $this->assets->collection('headerCss');
         $css->addCss('/assets/css/main.css');
